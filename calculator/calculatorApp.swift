ce1e1651771de658169ef5009ca008869a778f17
Odinachi David <odinachidavid@yahoo.com>
2024-04-29 12:24:44 +0100

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by  Apple on 29/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
