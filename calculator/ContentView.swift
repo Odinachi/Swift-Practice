d3b4d9a7f4ab1ea61243bc9b19472e9ae7306338
Odinachi David <odinachidavid@yahoo.com>
2024-04-29 14:13:37 +0100

    Designing button

--- calculator/ContentView.swift
+++ calculator/ContentView.swift
@@ -8,12 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttons = [
+           ["AC", "+/-", "%", "÷"],
+           ["7", "8", "9", "×"],
+           ["4", "5", "6", "-"],
+           ["1", "2", "3", "+"],
+           ["0", ".", "="],
+       ]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("000000000000")
+                .fontWeight(.medium)
+                .font(.system(size: 40)).frame(height: 300)
+                .frame( maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .bottomTrailing  )
+                .multilineTextAlignment(.trailing).padding(.bottom, 30)
+            
+            
+            VStack(spacing: 10) {
+                       ForEach(buttons.indices, id: \.self) { rowIndex in
+                           HStack(spacing: 10) {
+                               ForEach(buttons[rowIndex].indices, id: \.self) { columnIndex in
+                                   let buttonTitle = buttons[rowIndex][columnIndex]
+                                   Button(action: {
+                                       // Handle button tap
+                                       print("Tapped: \(buttonTitle)")
+                                   }) {
+                                       Text(buttonTitle)
+                                           .font(.custom("DM Sans", size: 30)).fontWeight(.heavy).padding()
+                                           .frame(width: 80, height: 80)
+                                           .background(rowIndex == 0 ? Color.primary.opacity(0.7): .white.opacity(0.2))
+                                           .foregroundColor(rowIndex == 0 ? .black : .white)
+                                           .cornerRadius(50)
+                                   }
+                               }
+                           }
+                       }
+                   }
+                   .padding()
+            Spacer()
         }
         .padding()
     }
